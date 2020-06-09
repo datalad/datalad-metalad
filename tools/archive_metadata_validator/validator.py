a7bf538344811b47107d7a07680ef8f04834eb1f
Christian Mönch <christian.moench@web.de>
2020-06-09 22:03:16 +0200

    RF: simplify the study metadata schema

--- tools/archive_metadata_validator/validator.py
+++ tools/archive_metadata_validator/validator.py
@@ -6,24 +6,13 @@ from typing import Any, List
 from yaml.error import YAMLError, MarkedYAMLError
 
 from content_validators.content_validator import ContentValidator
-from jsonschema import validate, ValidationError
+from jsonschema import Draft7Validator
 
 
 Typedef = namedtuple("SchemaTypedef", ["id", "schema"])
 
 
 class SpecValidator(object):
-    @staticmethod
-    def _create_schema_from_spec(definitions):
-        schema_creator = Rx.Factory({"register_core_types": True})
-        tuple(
-            map(
-                lambda typedef: schema_creator.learn_type(typedef.id, typedef.schema),
-                (Typedef(assoc["id"], assoc["schema"]) for assoc in definitions.get("typedefs", []))
-            )
-        )
-        return schema_creator.make_schema(definitions["schema"])
-
     @staticmethod
     def _load_spec_object(path_to_schema_spec) -> Any:
         with open(str(path_to_schema_spec), "rt") as spec_token_stream:
@@ -36,15 +25,16 @@ class SpecValidator(object):
 
     def __init__(self, path_to_schema_spec: PosixPath, validators: List[ContentValidator]):
         self.schema = self._load_spec_object(path_to_schema_spec)
+        Draft7Validator.check_schema(self.schema)
+        self.draft7_validator = Draft7Validator(self.schema)
         self.content_validators = validators
         self.errors = []
 
     def _validate_spec(self, spec):
-        try:
-            validate(instance=spec, schema=self.schema)
-            return []
-        except ValidationError as error:
-            return [f"Schema error: in {'.'.join(map(str, error.absolute_path))}: {error.message}"]
+        errors = []
+        for error in self.draft7_validator.iter_errors(instance=spec):
+            errors.append(f"Schema error: in {'.'.join(map(str, error.absolute_path))}: {error.message}")
+        return errors
 
     def validate_spec_object(self, spec) -> bool:
         self.errors = self._validate_spec(spec)
