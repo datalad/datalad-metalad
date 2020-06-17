6d600af3f93366291a1905c4f973f6737b937895
Christian Mönch <christian.moench@web.de>
2020-06-17 17:30:04 +0200

    BF: fix check for dataset.keyword

--- tools/archive_metadata_validator/content_validators/keyword_validator.py
+++ tools/archive_metadata_validator/content_validators/keyword_validator.py
@@ -9,7 +9,7 @@ KEYWORD_LOOKUP_URL = "https://jugit.fz-juelich.de/c.moench/datasets_repo/-/wikis
 
 class KeywordValidator(ContentValidator):
     def perform_validation(self, spec: dict) -> List[ValidatorMessage]:
-        keywords = self.value_at("dataset.keywords", spec)
+        keywords = self.value_at("dataset.keyword", spec)
         if not keywords:
             return [ValidatorMessage("Warning: no keywords given for dataset, please consider adding some. "
                                      f"See <{KEYWORD_LOOKUP_URL}> for a list of possible keywords.",
