ce18b208897af8818162246c4c26d05d72c1560a
Christian Mönch <christian.moench@web.de>
2020-06-19 10:16:21 +0200

    RF: limit the number of individual ex-tag warnings

--- tools/archive_metadata_validator/content_validators/extag_validator.py
+++ tools/archive_metadata_validator/content_validators/extag_validator.py
@@ -1,11 +1,14 @@
-from typing import Dict, List, Union
+from typing import Dict, List, Tuple, Union
 
-from messages import ValidatorMessage, WarningMessage, ObjectLocation
+from messages import ValidatorMessage, WarningMessage, ObjectLocation, StringLocation
 from .content_validator import ContentValidator
 
 
+MAX_EXTAG_WARNINGS = 5
+
+
 class ExTagValidator(ContentValidator):
-    def _validate(self, spec: Union[Dict, List], prefix: str = "") -> List[ValidatorMessage]:
+    def _validate(self, spec: Union[Dict, List], prefix: str = "") -> List[Tuple[str, str]]:
         result = []
         if isinstance(spec, dict):
             for key, value in spec.items():
@@ -15,11 +18,22 @@ class ExTagValidator(ContentValidator):
                 result += self._validate(item, f"{prefix}[{index}]" if prefix else f"[{index}]")
         elif isinstance(spec, str):
             if spec.startswith("<ex>") or spec.endswith("</ex>"):
-                result += [
-                    WarningMessage(
-                        f"ex-tags around content ({spec}), did you forget to alter the example content?",
-                        ObjectLocation(self.file_name, prefix))]
+                result += [(spec, prefix)]
         return result
 
     def perform_validation(self, spec: dict) -> List[ValidatorMessage]:
-        return self._validate(spec)
+        extag_locations = self._validate(spec)
+        if len(extag_locations) <= MAX_EXTAG_WARNINGS:
+            return list([
+                WarningMessage(
+                    f"ex-tags in ({content}), did you forget to remove them?",
+                    ObjectLocation(self.file_name, dotted_name))
+                for content, dotted_name in extag_locations])
+        else:
+            example_content, example_dotted_name = extag_locations[0]
+            return [
+                WarningMessage(
+                    f"ex-tags found in {len(extag_locations)} text strings, "
+                    f"for example ``{example_dotted_name}: {example_content}´´, "
+                    f"did you forget to remove them?",
+                    StringLocation(f"{self.file_name}:<multiple places>"))]
