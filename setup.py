14f4565a2ea6e7aa5089268b442572235944ac30
Christian Mönch <christian.moench@web.de>
2021-02-09 13:11:51 +0100

    NF: add dataladmetadatatools to dependencies

--- setup.py
+++ setup.py
@@ -44,6 +44,7 @@ setup(
     packages=[pkg for pkg in find_packages('.') if pkg.startswith('datalad')],
     install_requires=[
         'datalad>=0.14',
+        'datalad-metadata-dataladmetadatamodel',
         'pyyaml'
     ],
     entry_points={
