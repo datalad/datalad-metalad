7d4f85a80301bf865bfbdba2498ae05fb3675a18
Christian Mönch <christian.moench@web.de>
2021-03-01 13:02:11 +0100

    RF: rename string constant classes
    String constant classes have been renamed to:
    a) better reflect their origin, i.e. schema.org
    b) ensure unique names to prevent clashes due to "import *"

--- datalad_metalad/extractors/studyminimeta/ldcreator.py
+++ datalad_metalad/extractors/studyminimeta/ldcreator.py
@@ -50,14 +50,16 @@ class DataladIdCategory:
     PUBLISHER = "publisher"
 
 
-class JsonLdTags:
+class SMMJsonLdTags:
+    CONTEXT = "@context"
     GRAPH = "@graph"
     ID = "@id"
     LIST = "@list"
     TYPE = "@type"
+    VOCAB = "@vocab"
 
 
-class JsonLdTypes:
+class SMMSchemaOrgTypes:
     CONTACT_POINT = "ContactPoint"
     CREATIVE_WORK = "CreativeWork"
     DATASET = "Dataset"
@@ -71,7 +73,7 @@ class JsonLdTypes:
     SCHOLARLY_ARTICLE = "ScholarlyArticle"
 
 
-class JsonLdProperties:
+class SMMSchemaOrgProperties:
     ABSTRACT = "abstract"
     ACCOUNTABLE_PERSON = "accountablePerson"
     AFFILIATION = "affiliation"
@@ -108,40 +110,40 @@ class JsonLdProperties:
 
 # Automatically translated study-element properties
 STUDY_TRANSLATION_TABLE = {
-    SMMProperties.NAME: JsonLdProperties.NAME,
-    SMMProperties.PRINCIPAL_INVESTIGATOR: JsonLdProperties.ACCOUNTABLE_PERSON,
-    SMMProperties.KEYWORD: JsonLdProperties.KEYWORDS,
-    SMMProperties.PURPOSE: JsonLdProperties.ABSTRACT,
-    SMMProperties.START_DATE: JsonLdProperties.DATE_CREATED
+    SMMProperties.NAME: SMMSchemaOrgProperties.NAME,
+    SMMProperties.PRINCIPAL_INVESTIGATOR: SMMSchemaOrgProperties.ACCOUNTABLE_PERSON,
+    SMMProperties.KEYWORD: SMMSchemaOrgProperties.KEYWORDS,
+    SMMProperties.PURPOSE: SMMSchemaOrgProperties.ABSTRACT,
+    SMMProperties.START_DATE: SMMSchemaOrgProperties.DATE_CREATED
 }
 
 
 # Automatically translated person-element properties
 PERSON_TRANSLATION_TABLE = {
-    SMMProperties.GIVEN_NAME: JsonLdProperties.GIVEN_NAME,
-    SMMProperties.LAST_NAME: JsonLdProperties.FAMILY_NAME,
-    SMMProperties.TITLE: JsonLdProperties.HONORIFIC_SUFFIX,
-    SMMProperties.AFFILIATION: JsonLdProperties.AFFILIATION,
-    SMMProperties.ORCID_ID: JsonLdProperties.SAME_AS
+    SMMProperties.GIVEN_NAME: SMMSchemaOrgProperties.GIVEN_NAME,
+    SMMProperties.LAST_NAME: SMMSchemaOrgProperties.FAMILY_NAME,
+    SMMProperties.TITLE: SMMSchemaOrgProperties.HONORIFIC_SUFFIX,
+    SMMProperties.AFFILIATION: SMMSchemaOrgProperties.AFFILIATION,
+    SMMProperties.ORCID_ID: SMMSchemaOrgProperties.SAME_AS
 }
 
 
 # Automatically translated publication-element properties (to https://schema.org/ScholarlyArticle)
 PUBLICATION_TRANSLATION_TABLE = {
-    SMMProperties.TITLE: JsonLdProperties.HEADLINE,
-    SMMProperties.YEAR: JsonLdProperties.DATE_PUBLISHED,
-    SMMProperties.DOI: JsonLdProperties.SAME_AS,
-    SMMProperties.PAGES: JsonLdProperties.PAGINATION,
-    SMMProperties.CORRESPONDING_AUTHOR: JsonLdProperties.ACCOUNTABLE_PERSON
+    SMMProperties.TITLE: SMMSchemaOrgProperties.HEADLINE,
+    SMMProperties.YEAR: SMMSchemaOrgProperties.DATE_PUBLISHED,
+    SMMProperties.DOI: SMMSchemaOrgProperties.SAME_AS,
+    SMMProperties.PAGES: SMMSchemaOrgProperties.PAGINATION,
+    SMMProperties.CORRESPONDING_AUTHOR: SMMSchemaOrgProperties.ACCOUNTABLE_PERSON
 }
 
 
 # Automatically translated dataset-element properties (to https://schema.org/Dataset)
 DATASET_TRANSLATION_TABLE = {
-    SMMProperties.NAME: JsonLdProperties.NAME,
-    SMMProperties.LOCATION: JsonLdProperties.URL,
-    SMMProperties.KEYWORD: JsonLdProperties.KEYWORDS,
-    SMMProperties.DESCRIPTION: JsonLdProperties.DESCRIPTION
+    SMMProperties.NAME: SMMSchemaOrgProperties.NAME,
+    SMMProperties.LOCATION: SMMSchemaOrgProperties.URL,
+    SMMProperties.KEYWORD: SMMSchemaOrgProperties.KEYWORDS,
+    SMMProperties.DESCRIPTION: SMMSchemaOrgProperties.DESCRIPTION
 }
 
 
@@ -191,52 +193,52 @@ class LDCreator(object):
         issue = publication.get(SMMProperties.ISSUE, None)
         if volume and issue:
             return {
-                JsonLdTags.ID: "#issue({issue})".format(issue=issue),
-                JsonLdTags.TYPE: JsonLdTypes.PUBLICATION_ISSUE,
-                JsonLdProperties.ISSUE_NUMBER: issue,
-                JsonLdProperties.IS_PART_OF: {
-                    JsonLdTags.ID: "#volume({volume})".format(volume=volume),
-                    JsonLdTags.TYPE: JsonLdTypes.PUBLICATION_VOLUME,
-                    JsonLdProperties.VOLUME_NUMBER: volume,
+                SMMJsonLdTags.ID: "#issue({issue})".format(issue=issue),
+                SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.PUBLICATION_ISSUE,
+                SMMSchemaOrgProperties.ISSUE_NUMBER: issue,
+                SMMSchemaOrgProperties.IS_PART_OF: {
+                    SMMJsonLdTags.ID: "#volume({volume})".format(volume=volume),
+                    SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.PUBLICATION_VOLUME,
+                    SMMSchemaOrgProperties.VOLUME_NUMBER: volume,
                 }
             }
         if issue:
             return {
-                JsonLdTags.ID: "#issue({issue})".format(issue=issue),
-                JsonLdTags.TYPE: JsonLdTypes.PUBLICATION_ISSUE,
-                JsonLdProperties.ISSUE_NUMBER: issue
+                SMMJsonLdTags.ID: "#issue({issue})".format(issue=issue),
+                SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.PUBLICATION_ISSUE,
+                SMMSchemaOrgProperties.ISSUE_NUMBER: issue
             }
         if volume:
             return {
-                JsonLdTags.ID: "#volume({volume})".format(volume=volume),
-                JsonLdTags.TYPE: JsonLdTypes.PUBLICATION_VOLUME,
-                JsonLdProperties.VOLUME_NUMBER: volume
+                SMMJsonLdTags.ID: "#volume({volume})".format(volume=volume),
+                SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.PUBLICATION_VOLUME,
+                SMMSchemaOrgProperties.VOLUME_NUMBER: volume
             }
         return {}
 
     def _create_study_ld(self, study: dict) -> dict:
         return {
-            JsonLdTags.ID: "#study",
-            JsonLdTags.TYPE: JsonLdTypes.CREATIVE_WORK,
+            SMMJsonLdTags.ID: "#study",
+            SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.CREATIVE_WORK,
             **self._get_translated_dict(study, STUDY_TRANSLATION_TABLE),
             **{
-                JsonLdProperties.DESCRIPTION: "end_date: {value}".format(value=value)
+                SMMSchemaOrgProperties.DESCRIPTION: "end_date: {value}".format(value=value)
                 for key, value in study.items() if key == SMMProperties.END_DATE
             },
             **{
-                JsonLdProperties.CONTRIBUTOR: [
+                SMMSchemaOrgProperties.CONTRIBUTOR: [
                     {
-                        JsonLdTags.ID: self._get_person_id(email),
+                        SMMJsonLdTags.ID: self._get_person_id(email),
                     } for email in value
                 ]
                 for key, value in study.items() if key == SMMProperties.CONTRIBUTOR
             },
             **{
-                JsonLdProperties.FUNDER: [
+                SMMSchemaOrgProperties.FUNDER: [
                     {
-                        JsonLdTags.ID: self._get_organization_id(funder_name),
-                        JsonLdTags.TYPE: JsonLdTypes.ORGANIZATION,
-                        JsonLdProperties.NAME: funder_name
+                        SMMJsonLdTags.ID: self._get_organization_id(funder_name),
+                        SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.ORGANIZATION,
+                        SMMSchemaOrgProperties.NAME: funder_name
                     }
                     for funder_name in study[SMMProperties.FUNDING]
                 ]
@@ -246,25 +248,25 @@ class LDCreator(object):
 
     def _create_dataset_ld(self, dataset: dict) -> dict:
         return {
-            JsonLdTags.ID: self._get_dataset_jsonld_id(self.dataset_id),
-            JsonLdTags.TYPE: JsonLdTypes.DATASET,
-            JsonLdProperties.VERSION: self.ref_commit,
+            SMMJsonLdTags.ID: self._get_dataset_jsonld_id(self.dataset_id),
+            SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.DATASET,
+            SMMSchemaOrgProperties.VERSION: self.ref_commit,
             **self._get_translated_dict(dataset, DATASET_TRANSLATION_TABLE),
             **{
-                JsonLdProperties.AUTHOR: [
+                SMMSchemaOrgProperties.AUTHOR: [
                     {
-                        JsonLdTags.ID: self._get_person_id(email),
+                        SMMJsonLdTags.ID: self._get_person_id(email),
                     } for email in email_list
                 ]
                 for key, email_list in dataset.items()
                 if key == SMMProperties.AUTHOR
             },
             **{
-                JsonLdProperties.FUNDER: [
+                SMMSchemaOrgProperties.FUNDER: [
                     {
-                        JsonLdTags.ID: self._get_organization_id(funder_name),
-                        JsonLdTags.TYPE: JsonLdTypes.ORGANIZATION,
-                        JsonLdProperties.NAME: funder_name
+                        SMMJsonLdTags.ID: self._get_organization_id(funder_name),
+                        SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.ORGANIZATION,
+                        SMMSchemaOrgProperties.NAME: funder_name
                     }
                     for funder_name in dataset[SMMProperties.FUNDING]
                 ]
@@ -272,14 +274,14 @@ class LDCreator(object):
                 if key == SMMProperties.FUNDING
             },
             **{
-                JsonLdProperties.HAS_PART: {
-                    JsonLdTags.ID: "#standards",
-                    JsonLdTags.TYPE: JsonLdTypes.DEFINED_TERM_SET,
-                    JsonLdProperties.HAS_DEFINED_TERM: [
+                SMMSchemaOrgProperties.HAS_PART: {
+                    SMMJsonLdTags.ID: "#standards",
+                    SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.DEFINED_TERM_SET,
+                    SMMSchemaOrgProperties.HAS_DEFINED_TERM: [
                         {
-                            JsonLdTags.ID: self._get_datalad_global_id("standard", standard),
-                            JsonLdTags.TYPE: JsonLdTypes.DEFINED_TERM,
-                            JsonLdProperties.TERM_CODE: standard
+                            SMMJsonLdTags.ID: self._get_datalad_global_id("standard", standard),
+                            SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.DEFINED_TERM,
+                            SMMSchemaOrgProperties.TERM_CODE: standard
                         }
                         for standard in dataset[SMMProperties.STANDARD]
                     ]
@@ -288,7 +290,7 @@ class LDCreator(object):
                 if SMMProperties.STANDARD in dataset
             },
             **{
-                JsonLdProperties.DESCRIPTION:
+                SMMSchemaOrgProperties.DESCRIPTION:
                     "<this is an autogenerated description for dataset {dataset_id} ,"
                     "since no description was provided by the author, and "
                     "because google rich-results requires the description-property "
@@ -301,38 +303,38 @@ class LDCreator(object):
     def _create_publication_list_ld(self, publication_list: list) -> list:
         return [
             {
-                JsonLdTags.ID: "#publication[{publication_index}]".format(publication_index=publication_index),
-                JsonLdTags.TYPE: JsonLdTypes.SCHOLARLY_ARTICLE,
+                SMMJsonLdTags.ID: "#publication[{publication_index}]".format(publication_index=publication_index),
+                SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.SCHOLARLY_ARTICLE,
                 **self._get_translated_dict(publication, PUBLICATION_TRANSLATION_TABLE),
                 **{
-                    JsonLdProperties.AUTHOR: [
+                    SMMSchemaOrgProperties.AUTHOR: [
                         {
-                            JsonLdTags.ID: self._get_person_id(email)
+                            SMMJsonLdTags.ID: self._get_person_id(email)
                         } for email in email_list
                     ]
                     for key, email_list in publication.items()
                     if key == SMMProperties.AUTHOR
                 },
                 **{
-                    JsonLdProperties.PUBLISHER: {
-                        JsonLdTags.ID: self._get_publisher_id(publisher_name),
-                        JsonLdTags.TYPE: JsonLdTypes.ORGANIZATION,
-                        JsonLdProperties.NAME: publisher_name
+                    SMMSchemaOrgProperties.PUBLISHER: {
+                        SMMJsonLdTags.ID: self._get_publisher_id(publisher_name),
+                        SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.ORGANIZATION,
+                        SMMSchemaOrgProperties.NAME: publisher_name
                     }
                     for key, publisher_name in publication.items()
                     if key == SMMProperties.PUBLISHER
                 },
                 **{
-                    JsonLdProperties.PUBLICATION: {
-                        JsonLdTags.ID: self._get_datalad_global_id("publication_event", publication_event_name),
-                        JsonLdTags.TYPE: JsonLdTypes.PUBLICATION_EVENT,
-                        JsonLdProperties.NAME: publication_event_name
+                    SMMSchemaOrgProperties.PUBLICATION: {
+                        SMMJsonLdTags.ID: self._get_datalad_global_id("publication_event", publication_event_name),
+                        SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.PUBLICATION_EVENT,
+                        SMMSchemaOrgProperties.NAME: publication_event_name
                     }
                     for key, publication_event_name in publication.items()
                     if key == SMMProperties.PUBLICATION
                 },
                 **{
-                    JsonLdProperties.DESCRIPTION: "{corresponding_author}: {publication}".format(
+                    SMMSchemaOrgProperties.DESCRIPTION: "{corresponding_author}: {publication}".format(
                         corresponding_author=SMMProperties.CORRESPONDING_AUTHOR,
                         publication=publication[SMMProperties.CORRESPONDING_AUTHOR]
                     )
@@ -340,7 +342,7 @@ class LDCreator(object):
                     if SMMProperties.CORRESPONDING_AUTHOR in publication
                 },
                 **{
-                    JsonLdProperties.IS_PART_OF: {
+                    SMMSchemaOrgProperties.IS_PART_OF: {
                         **self._get_volume_issue_dict(publication_index, publication)
                     }
                     for _ in [0]
@@ -352,16 +354,16 @@ class LDCreator(object):
     def _create_person_list_ld(self, persons: dict) -> list:
         return [
             {
-                JsonLdTags.ID: self._get_person_id(email),
-                JsonLdTags.TYPE: JsonLdTypes.PERSON,
-                JsonLdProperties.EMAIL: email,
-                JsonLdProperties.NAME: self._get_person_name(details),
+                SMMJsonLdTags.ID: self._get_person_id(email),
+                SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.PERSON,
+                SMMSchemaOrgProperties.EMAIL: email,
+                SMMSchemaOrgProperties.NAME: self._get_person_name(details),
                 **self._get_translated_dict(details, PERSON_TRANSLATION_TABLE),
                 **{
-                    JsonLdProperties.CONTACT_POINT: {
-                        JsonLdTags.ID: "#contactPoint({email})".format(email=email),
-                        JsonLdTags.TYPE: JsonLdTypes.CONTACT_POINT,
-                        JsonLdProperties.DESCRIPTION: details[SMMProperties.CONTACT_INFORMATION]
+                    SMMSchemaOrgProperties.CONTACT_POINT: {
+                        SMMJsonLdTags.ID: "#contactPoint({email})".format(email=email),
+                        SMMJsonLdTags.TYPE: SMMSchemaOrgTypes.CONTACT_POINT,
+                        SMMSchemaOrgProperties.DESCRIPTION: details[SMMProperties.CONTACT_INFORMATION]
                     }
                     for _ in [0]
                     if SMMProperties.CONTACT_INFORMATION in details
@@ -380,22 +382,22 @@ class LDCreator(object):
             for key, sub_spec in spec.items()
         }
 
-        graph_elements[JsonLdProperties.PERSON] = {
-            JsonLdTags.ID: "#personList",
-            JsonLdTags.LIST: graph_elements[JsonLdProperties.PERSON]
+        graph_elements[SMMSchemaOrgProperties.PERSON] = {
+            SMMJsonLdTags.ID: "#personList",
+            SMMJsonLdTags.LIST: graph_elements[SMMSchemaOrgProperties.PERSON]
         }
 
-        if JsonLdProperties.PUBLICATION in graph_elements:
-            graph_elements[JsonLdProperties.PUBLICATION] = {
-                JsonLdTags.ID: "#publicationList",
-                JsonLdTags.LIST: graph_elements[JsonLdProperties.PUBLICATION]
+        if SMMSchemaOrgProperties.PUBLICATION in graph_elements:
+            graph_elements[SMMSchemaOrgProperties.PUBLICATION] = {
+                SMMJsonLdTags.ID: "#publicationList",
+                SMMJsonLdTags.LIST: graph_elements[SMMSchemaOrgProperties.PUBLICATION]
             }
 
         json_ld = {
-            "@context": {
-                "@vocab": "http://schema.org/",
+            SMMJsonLdTags.CONTEXT: {
+                SMMJsonLdTags.VOCAB: "http://schema.org/",
             },
-            JsonLdTags.GRAPH: [value for _, value in graph_elements.items()]
+            SMMJsonLdTags.GRAPH: [value for _, value in graph_elements.items()]
         }
         return LDCreatorResult(True, json_ld, [value for _, value in graph_elements.items()], [])
 
