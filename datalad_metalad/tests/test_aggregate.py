e08fd373bde00134ffbd839a8a3b3a155bd1d4c5
Christian Mönch <christian.moench@web.de>
2021-04-15 16:49:32 +0200

    NF: add test for non-existing root

--- datalad_metalad/tests/test_aggregate.py
+++ datalad_metalad/tests/test_aggregate.py
@@ -130,3 +130,68 @@ def test_missing_metadata_stores():
                 "subdataset_0", str(subdataset_0_dir),
                 "subdataset_1", str(subdataset_1_dir)
             ])
+
+
+def test_basic_aggregation_into_empty_store():
+
+    with tempfile.TemporaryDirectory() as root_dataset_dir_str:
+        root_dataset_dir = Path(root_dataset_dir_str)
+        subdataset_0_dir = root_dataset_dir / "subdataset_0"
+        subdataset_1_dir = root_dataset_dir / "subdataset_1"
+
+        root_git_repo = GitRepo(root_dataset_dir)
+        subdataset_0_repo = GitRepo(subdataset_0_dir)
+        subdataset_1_repo = GitRepo(subdataset_1_dir)
+
+        # TODO: there is a dependency here in meta_add. We should instead
+        #  use the model API to add metadata to metadata stores
+
+        for index in range(2):
+            _add_dataset_level_metadata(
+                [
+                    subdataset_0_dir,
+                    subdataset_1_dir
+                ][index],
+                f"00000000-0000-0000-0000-00000000000{index}",
+                f"000000000000000000000000000000000000000{index}",
+                f"metadata-content_{index}")
+
+        # We have to patch "get_root_version_for_subset_version" because
+        # the test git repos have no commits.
+        with patch("datalad_metalad.aggregate.get_root_version_for_subset_version") as p:
+
+            # Ensure that the root version is found
+            p.return_value = ["0000000000000000000000000000000000000aaa"]
+
+            result = meta_aggregate(
+                str(root_dataset_dir),
+                [
+                    "subdataset_0", str(subdataset_0_dir),
+                    "subdataset_1", str(subdataset_1_dir)
+                ])
+
+            result_objects = meta_dump(
+                metadata_store=str(root_dataset_dir),
+                recursive=True)
+
+            assert_result_count(result_objects, 2)
+            for index, result in enumerate(result_objects):
+                result_object = result["metadata"]["dataset_level_metadata"]
+                eq_(result_object["root_dataset_identifier"], "<unknown>")
+                eq_(result_object["root_dataset_version"], "0000000000000000000000000000000000000aaa")
+
+                eq_(result_object["dataset_identifier"],
+                    f"00000000-0000-0000-0000-00000000000{index}")
+
+                eq_(result_object["dataset_version"],
+                    f"000000000000000000000000000000000000000{index}")
+
+                eq_(result_object["dataset_path"], [
+                    "subdataset_0",
+                    "subdataset_1"
+                ][index])
+
+                metadata_content = result_object["metadata"]["test_dataset"][0]
+
+                eq_(metadata_content["extraction_result"]["content"],
+                    f"metadata-content_{index}")
