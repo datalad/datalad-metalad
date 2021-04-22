7b7f1c700285573239c186b9d21626afa02b6352
Christian Mönch <christian.moench@web.de>
2021-04-22 13:25:51 +0200

    RF: remove metadata-store from dump UI

--- datalad_metalad/tests/test_aggregate.py
+++ datalad_metalad/tests/test_aggregate.py
@@ -18,34 +18,12 @@ from datalad.api import meta_add, meta_aggregate, meta_dump
 from datalad.support.exceptions import InsufficientArgumentsError
 from datalad.tests.utils import assert_raises, assert_result_count, eq_
 
-from .utils import create_dataset
+from .utils import add_dataset_level_metadata, create_dataset
 
 
 root_id = UUID("00010203-1011-2021-3031-404142434445")
 sub_0_id = UUID("a0cc0203-1011-2021-3031-404142434445")
 sub_1_id = UUID("a1cc0203-1011-2021-3031-404142434445")
-
-
-def _add_dataset_level_metadata(metadata_store: Path,
-                                dataset_id: str,
-                                dataset_version: str,
-                                metadata_content: str):
-    meta_add(
-        {
-            "type": "dataset",
-            "extractor_name": "test_dataset",
-            "extractor_version": "1.0",
-            "extraction_parameter": {},
-            "extraction_time": "1000.1",
-            "agent_name": "test_aggregate",
-            "agent_email": "test@test.aggregate",
-            "dataset_id": dataset_id,
-            "dataset_version": dataset_version,
-            "extracted_metadata": {
-                "content": metadata_content
-            }
-        },
-        dataset=metadata_store)
 
 
 def test_basic_aggregation():
@@ -55,15 +33,15 @@ def test_basic_aggregation():
         subdataset_0_dir = root_dataset_dir / "subdataset_0"
         subdataset_1_dir = root_dataset_dir / "subdataset_1"
 
-        root_git_repo = create_dataset(root_dataset_dir, root_id)
-        subdataset_0_repo = create_dataset(subdataset_0_dir, sub_0_id)
-        subdataset_1_repo = create_dataset(subdataset_1_dir, sub_1_id)
+        create_dataset(root_dataset_dir, root_id)
+        create_dataset(subdataset_0_dir, sub_0_id)
+        create_dataset(subdataset_1_dir, sub_1_id)
 
         # TODO: there is a dependency here in meta_add. We should instead
         #  use the model API to add metadata to metadata stores
 
         for index in range(3):
-            _add_dataset_level_metadata(
+            add_dataset_level_metadata(
                 metadata_store=[
                     root_dataset_dir,
                     subdataset_0_dir,
@@ -92,7 +70,7 @@ def test_basic_aggregation():
                 ])
 
             result_objects = meta_dump(
-                metadata_store=str(root_dataset_dir),
+                dataset=str(root_dataset_dir),
                 recursive=True)
 
             assert_result_count(result_objects, 3)
@@ -157,11 +135,11 @@ def test_basic_aggregation_into_empty_store():
         create_dataset(subdataset_1_dir, sub_1_id)
 
         # TODO: this is more an end-to-end test, since we depend
-        #  meta_add. We should instead use the model API to add
+        #  on meta_add. We should instead use the model API to add
         #  metadata to metadata stores
 
         for index in range(2):
-            _add_dataset_level_metadata(
+            add_dataset_level_metadata(
                 metadata_store=[
                     subdataset_0_dir,
                     subdataset_1_dir
@@ -188,7 +166,7 @@ def test_basic_aggregation_into_empty_store():
                 ])
 
             result_objects = meta_dump(
-                metadata_store=str(root_dataset_dir),
+                dataset=str(root_dataset_dir),
                 recursive=True)
 
             assert_result_count(result_objects, 2)
