23844872f448fe85041f1198c08e300910bcb2d7
Christian Mönch <christian.moench@web.de>
2021-10-11 10:15:19 +0200

    test multiple aaders with identical names

--- datalad_metalad/tests/test_conduct.py
+++ datalad_metalad/tests/test_conduct.py
@@ -10,6 +10,7 @@
 import tempfile
 import uuid
 from dataclasses import dataclass
+from itertools import chain
 from pathlib import Path
 
 from datalad.api import meta_conduct
@@ -60,30 +61,24 @@ extract_pipeline = {
     ]
 }
 
-simple_pipeline = {
-    "provider": {
-        "name": "testprovider",
-        "module": "datalad_metalad.tests.test_conduct",
-        "class": "TestTraverser",
-        "arguments": [],
-        "keyword_arguments": {}
-    },
-    "processors": [
-        {
-            "name": f"testproc{index}",
-            "module": "datalad_metalad.tests.test_conduct",
-            "class": "PathEater",
-            "arguments": [],
-            "keyword_arguments": {}
-        }
-        for index in range(3)
-    ]
+
+test_provider = {
+    "name": "testprovider",
+    "module": "datalad_metalad.tests.test_conduct",
+    "class": "TestTraverser",
+    "arguments": [],
+    "keyword_arguments": {}
 }
 
 
 @dataclass
 class TestResult(PipelineResult):
     path: Path
+
+
+@dataclass
+class StringResult(PipelineResult):
+    content: str
 
 
 class TestTraverser(Provider):
@@ -114,7 +109,39 @@ class PathEater(Processor):
         return pipeline_element
 
 
+class DataAdder(Processor):
+    """
+    Add a StringPipelineResult with the string
+    `data` and the
+    """
+    def __init__(self, source_name: str, content: str):
+        super().__init__()
+        self.source_name = source_name
+        self.content = content
+
+    def process(self, pipeline_element: PipelineElement) -> PipelineElement:
+        pipeline_element.add_result(
+            self.source_name,
+            StringResult(ResultState.SUCCESS, self.content)
+        )
+        return pipeline_element
+
+
 def test_simple_pipeline():
+    simple_pipeline = {
+        "provider": test_provider,
+        "processors": [
+            {
+                "name": f"testproc{index}",
+                "module": "datalad_metalad.tests.test_conduct",
+                "class": "PathEater",
+                "arguments": [],
+                "keyword_arguments": {}
+            }
+            for index in range(3)
+        ]
+    }
+
     pipeline_results = list(
         meta_conduct(
             arguments=[f"testprovider:a/b/c:/d/e/f:/a/b/x:a/b/y"],
@@ -132,9 +159,9 @@ def test_extract():
         subdataset_0_dir = root_dataset_dir / "subdataset_0"
         subdataset_1_dir = root_dataset_dir / "subdataset_1"
 
-        create_dataset(root_dataset_dir, uuid.uuid4())
-        create_dataset(subdataset_0_dir, uuid.uuid4())
-        create_dataset(subdataset_1_dir, uuid.uuid4())
+        create_dataset(str(root_dataset_dir), uuid.uuid4())
+        create_dataset(str(subdataset_0_dir), uuid.uuid4())
+        create_dataset(str(subdataset_1_dir), uuid.uuid4())
 
         pipeline_results = list(
             meta_conduct(
@@ -147,3 +174,41 @@ def test_extract():
 
         eq_(len(pipeline_results), 1)
         assert_equal(pipeline_results[0]["status"], "ok")
+
+
+def test_multiple_adder():
+    adder_pipeline = {
+        "provider": test_provider,
+        "processors": [
+            {
+                "name": f"adder{index}",
+                "module": "datalad_metalad.tests.test_conduct",
+                "class": "DataAdder",
+                "arguments": [],
+                "keyword_arguments": {}
+            }
+            for index in range(3)
+        ]
+    }
+
+    adder_count = 3
+    source_name = "adder-data"
+    pipeline_results = list(
+        meta_conduct(
+            arguments=[
+                "testprovider:a/b/c",
+                *list(chain(*[
+                    [f"adder{index}:{source_name}", f"adder{index}:content from adder {index}"]
+                    for index in range(adder_count)
+                ]))
+            ],
+            configuration=adder_pipeline))
+
+    eq_(len(pipeline_results), 1)
+    result = pipeline_results[0]
+    assert_equal(result["status"], "ok")
+    pipeline_element = result["pipeline_element"]
+    adder_results = pipeline_element.get_result("adder-data")
+    assert_equal(len(adder_results), adder_count)
+    for i in range(adder_count):
+        assert_equal(adder_results[i].content, f"content from adder {i}")
