15b11687ae5cae6ac46c363e3855b87559ac188f
Christian Monch <christian.moench@web.de>
2022-07-29 14:57:13 +0200

    use dataclasses for annex-status info
    This commit introduces AnnexedFileInfo,
    to hold annex-status information for a
    single file. To simplify handling,
    the dataclasses_json package is used
    and added to requirements
    
    Python version requirement has been
    set to >=3.7

--- datalad_metalad/extractors/base.py
+++ datalad_metalad/extractors/base.py
@@ -7,9 +7,12 @@
 #
 # ## ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ### ##
 """MetadataRecord extractor base class"""
+from __future__ import annotations
+
 import abc
-import dataclasses
 import enum
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
 from typing import (
     Any,
     Dict,
@@ -24,17 +27,78 @@ from uuid import UUID
 from datalad.distribution.dataset import Dataset
 
 
-@dataclasses.dataclass
+@dataclass_json
+@dataclass
 class FileInfo:
-    type: str           # TODO: state constants
-    git_sha_sum: str
-    byte_size: int
-    state: str          # TODO: state constants
     path: str
     intra_dataset_path: str
+    type: str           # TODO: state constants
+    git_sha_sum: str
+    prev_git_sha_sum: str
+    byte_size: int
+    state: str          # TODO: state constants
+
+    def to_legacy_dict(self) -> dict:
+        return {
+            self.path: {
+                "type": self.type,
+                "gitshasum": self.git_sha_sum,
+                "prev_gitshasum": self.prev_git_sha_sum,
+                'bytesize': self.byte_size,
+                "state": self.state
+            }
+        }
+
+    def from_legacy_dict(self, legacy_dict: dict):
+        assert len(legacy_dict) == 1, "legacy info dict has more than one key"
+        self.path, content = tuple(legacy_dict.items())[0]
+        self.type = legacy_dict["type"]
+        self.git_sha_sum = legacy_dict["gitshasum"]
+        self.prev_git_sha_sum = legacy_dict["prev_gitshasum"]
+        self.byte_size = legacy_dict["bytesize"]
+        self.state = legacy_dict["state"]
 
 
-@dataclasses.dataclass
+@dataclass_json
+@dataclass
+class AnnexedFileInfo(FileInfo):
+    human_size: str
+    key: str
+    backend: str
+    hash_dir_lower: str
+    hash_dir_mixed: str
+    mtime: str
+    key_name: str
+    has_content: bool
+
+    def to_legacy_dict(self) -> dict:
+        return {
+            self.path: {
+                **FileInfo.legacy_dict(self)[self.path],
+                'humansize': self.human_size,
+                'key': self.key,
+                'backend': self.backend,
+                'hashdirlower': self.hash_dir_lower,
+                'mtime': self.mtime,
+                'hashdirmixed': self.hash_dir_mixed,
+                'keyname': self.key_name,
+                'has_content': self.has_content
+            }
+        }
+
+    def from_legacy_dict(self, legacy_dict: dict):
+        FileInfo.from_legacy_dict(self, legacy_dict)
+        self.human_size = legacy_dict["humansize"]
+        self.key = legacy_dict["key"]
+        self.key_name = legacy_dict["keyname"]
+        self.backend = legacy_dict["backend"]
+        self.hash_dir_lower = legacy_dict["hashdirlower"]
+        self.hash_dir_mixed = legacy_dict["hashdirmixed"]
+        self.mtime = legacy_dict["mtime"]
+        self.has_content = legacy_dict["has_content"]
+
+
+@dataclass
 class ExtractorResult:
     extractor_version: str
     extraction_parameter: Dict[str, Any]
