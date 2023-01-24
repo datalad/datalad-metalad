647b68b3be8a8ec0eb19e1338ba251171c3a77e7
Christian Monch <christian.moench@web.de>
2023-01-24 15:50:33 +0100

    replace deprecated eval_results decorator

--- datalad_metalad/conduct.py
+++ datalad_metalad/conduct.py
@@ -31,9 +31,11 @@ from typing import (
 )
 
 from datalad.distribution.dataset import datasetmethod
-from datalad.interface.base import build_doc
-from datalad.interface.base import Interface
-from datalad.interface.utils import eval_results
+from datalad.interface.base import (
+    Interface,
+    build_doc,
+    eval_results,
+)
 from datalad.support.constraints import (
     EnsureChoice,
     EnsureInt,
