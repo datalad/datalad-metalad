75b8088321fd5a2288aafb6c33a30392bbc018d0
Christian Mönch <christian.moench@web.de>
2023-06-14 06:19:02 +0200

    use xargs instead of subprocess

--- iter_test/extract_adaptor.py
+++ iter_test/extract_adaptor.py
@@ -4,7 +4,6 @@ from __future__ import annotations
 
 import argparse
 import json
-import subprocess
 import sys
 from pathlib import Path
 
@@ -25,13 +24,13 @@ def main():
     for line in sys.stdin.readlines():
         file_info = json.loads(line)
         fs_base_path = Path(file_info['fs_base_path'])
-        cmd_line = ['datalad', 'meta-extract', '-d', str(fs_base_path / file_info['dataset_path'])]
+        cmd_line = ['-d', "'" + str(fs_base_path / file_info['dataset_path']) + "'"]
         if file_info['type'] == 'file':
-            cmd_line.extend(['--file-info', line])
-        cmd_line.append(arguments.extractor_name)
+            cmd_line.extend(['--file-info', "'" + line + "'"])
+        cmd_line.append("'" + arguments.extractor_name + "'")
         if file_info['type'] == 'file':
-            cmd_line.append(file_info['path'])
-        subprocess.run(cmd_line, check=True)
+            cmd_line.append("'" + file_info['path'] + "'")
+        print (' '.join(cmd_line))
 
 
 if __name__ == '__main__':
